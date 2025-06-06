a93e80d0e52d8d3cfccbd044d6a59d282e8f2a61
Fabiano Alves <fabianf14@outlook.com>
2025-06-06 10:24:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const usuario = document.getElementById('usuario').value;
     const senha = document.getElementById('senha').value;
   
-    // Exemplo de login simulado
     if (usuario === 'admin' && senha === '1234') {
       alert('Login bem-sucedido!');
-      // Redireciona para outra página
+
       window.location.href = 'dashboard.html';
     } else {
       alert('Usuário ou senha inválidos.');
